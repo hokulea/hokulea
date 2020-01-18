c2c255194d241279a6686a80b9225611098a218c
gossi <gossi@users.noreply.github.com>
2020-01-18 19:19:40 +0100

    cards

--- explorer/tests/dummy/app/router.js
+++ explorer/tests/dummy/app/router.js
@@ -1,5 +1,5 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -7,4 +7,6 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route("buttons");
+  this.route("cards");
 });

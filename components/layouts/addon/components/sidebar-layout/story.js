d3d521c4d3746558d6c53a72dc9b263460db1533
gossi <gossi@users.noreply.github.com>
2020-02-18 20:53:28 +0100

    Update sidebar layout

---
+++ components/layouts/addon/components/sidebar-layout/story.js
@@ -0,0 +1,58 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+import { radios, text, withKnobs } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Components|Layouts/Sidebar'
+};
+
+export const basic = () => {
+  return {
+    template: hbs`
+      <SidebarLayout @min={{this.min}} as |s|>
+        {{#if this.left}}
+          <BoxLayout {{style flexBasis=(if this.base this.base "")}}>
+            Side
+          </BoxLayout>
+        {{/if}}
+        <BoxLayout class={{s.contentClass}}>
+          Only Content
+        </BoxLayout>
+        {{#if this.right}}
+          <BoxLayout {{style flexBasis=(if this.base this.base "")}}>
+            Side
+          </BoxLayout>
+        {{/if}}
+      </SidebarLayout>`,
+    context: {
+      get base() {
+        return text('base', '300px');
+      },
+
+      get min() {
+        return text('min', '50%');
+      },
+
+      get left() {
+        return (
+          radios(
+            'position',
+            {
+              left: 'left',
+              right: 'right'
+            },
+            'left'
+          ) === 'left'
+        );
+      },
+
+      get right() {
+        return !this.left;
+      }
+    }
+  };
+};
+
+basic.story = {
+  decorators: [withKnobs]
+};

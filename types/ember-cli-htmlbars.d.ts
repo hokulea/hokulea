8db08e6160056f8fc91159c35f0493804f24d6e7
gossi <gossi@users.noreply.github.com>
2020-04-11 08:58:39 +0200

    Update dependencies

--- types/ember-cli-htmlbars.d.ts
+++
@@ -1,4 +0,0 @@
-// for the time being until this issue is solved:
-// https://github.com/typed-ember/ember-cli-typescript/issues/983
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function hbs(tagged: TemplateStringsArray): any;

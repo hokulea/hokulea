25cba00153b0ad97e468ac268b692034e76ece63
gossi <gossi@users.noreply.github.com>
2020-05-10 15:14:15 +0200

    Add api-extractor

---
+++ packages/buttons/addon/api.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Hokulea buttons package
+ *
+ * @packageDocumentation
+ */
+
+export { default as AdjacentButtonComponent } from '@hokulea/buttons/components/adjacent-button';
+export { default as AccentButtonComponent } from '@hokulea/buttons/components/accent-button';
+export { default as ButtonComponent } from '@hokulea/buttons/components/button';
+export { default as DangerButtonComponent } from '@hokulea/buttons/components/danger-button';
+export { default as GhostButtonComponent } from '@hokulea/buttons/components/ghost-button';

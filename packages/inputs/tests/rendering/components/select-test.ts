c85ce9b7b91f1baca404e0b319c4c9bee8ff245b
gossi <gossi@users.noreply.github.com>
2021-02-27 11:55:17 +0100

    named blocks and tests

--- packages/inputs/tests/rendering/components/select-test.ts
+++ packages/inputs/tests/rendering/components/select-test.ts
@@ -22,23 +22,8 @@ interface TestContext extends BaseTestContext {
 module('Rendering | Component | <Select>', hooks => {
   setupRenderingTest(hooks);
 
-  // eslint-disable-next-line qunit/require-expect
-  test('it renders properly', async function (assert) {
-    await render(hbs`<Select></Select>`);
-
-    assert.dom(SelectPageObject.root).exists();
-    assert.dom(SelectPageObject.trigger).exists();
-    assert.dom(SelectPageObject.trigger).hasAria('expanded', 'false');
-    assert.dom(SelectPageObject.list).exists();
-    assert.dom(SelectPageObject.list).hasAttribute('tabindex', '-1');
-    assert.dom(SelectPageObject.list).hasStyle({
-      opacity: '0'
-    });
-  });
-
-  // eslint-disable-next-line qunit/require-expect
   test('it opens', async function (assert) {
-    await render(hbs`<Select></Select>`);
+    await render(hbs`<Select/>`);
     await SelectPageObject.open();
 
     assert.dom(SelectPageObject.trigger).hasAria('expanded', 'true');
@@ -47,74 +32,77 @@ module('Rendering | Component | <Select>', hooks => {
     });
   });
 
-  test('options are rendering', async function (this: TestContext, assert) {
-    this.options = ['apple', 'banana', 'pineapple'];
-    await render(hbs`
-      <Select
-        @options={{this.options}}
-        as |option|
-      >
-        {{option}}
-      </Select>
-    `);
-    await SelectPageObject.open();
+  module('Single Selection', () => {
+    test('it renders properly', async function (assert) {
+      await render(hbs`<Select/>`);
 
-    assert.dom(SelectPageObject.option).exists({ count: 3 });
-  });
-
-  test('select is invoked', async function (this: TestContext, assert) {
-    this.options = ['apple', 'banana', 'pineapple'];
-    this.select = sinon.spy();
-    await render(hbs`
-      <Select
-        @options={{this.options}}
-        @update={{this.select}}
-        as |option|
-      >
-        {{option}}
-      </Select>
-    `);
-    await SelectPageObject.select('apple');
-
-    assert.ok(this.select.calledOnceWith('apple'));
-  });
-
-  // eslint-disable-next-line qunit/require-expect
-  test('it supports keyboard controls for open and close', async function (this: TestContext, assert) {
-    await render(hbs`<Select/>`);
-    await testSelectKeyboardOpenAndClose(assert, {
-      trigger: SelectPageObject.trigger,
-      list: SelectPageObject.list
+      assert.dom(SelectPageObject.root).exists();
+      assert.dom(SelectPageObject.trigger).exists();
+      assert.dom(SelectPageObject.trigger).hasAria('expanded', 'false');
+      assert.dom(SelectPageObject.list).exists();
+      assert.dom(SelectPageObject.list).hasAttribute('tabindex', '-1');
+      assert.dom(SelectPageObject.list).hasStyle({
+        opacity: '0'
+      });
     });
-  });
 
-  // eslint-disable-next-line qunit/require-expect
-  test('it supports keyboard navigation', async function (this: TestContext, assert) {
-    this.options = ['apple', 'banana', 'pineapple'];
-    await render(hbs`<Select @options={{this.options}}/>`);
-    await testSelecKeyboardNavigation(assert, {
-      trigger: SelectPageObject.trigger,
-      list: SelectPageObject.list
+    test('named blocks are used', async function (this: TestContext, assert) {
+      this.options = ['apple', 'banana', 'pineapple'];
+      await render(hbs`
+        <Select @options={{this.options}}>
+          <:placeholder>Please select something</:placeholder>
+          <:option as |option|>{{option}}</:option>
+          <:selected as |value|>{{value}}</:selected>
+        </Select>
+      `);
+      await SelectPageObject.open();
+
+      assert.dom(SelectPageObject.option).exists({ count: 3 });
     });
-  });
 
-  // eslint-disable-next-line qunit/require-expect
-  test('it supports keyboard selection (single)', async function (this: TestContext, assert) {
-    this.options = ['apple', 'banana', 'pineapple'];
-    await render(hbs`<Select @options={{this.options}}/>`);
-    await testSelectKeyboardSelection(assert, {
-      trigger: SelectPageObject.trigger,
-      list: SelectPageObject.list
+    test('options are rendering as is', async function (this: TestContext, assert) {
+      this.options = ['apple', 'banana', 'pineapple'];
+      await render(hbs`<Select @options={{this.options}}/>`);
+      await SelectPageObject.open();
+
+      assert.dom(SelectPageObject.option).exists({ count: 3 });
     });
-  });
 
-  // eslint-disable-next-line qunit/require-expect
-  test('it supports keyboard selection (multi)', async function (this: TestContext, assert) {
-    this.options = ['apple', 'banana', 'pineapple'];
-    await render(hbs`<Select @multiple={{true}} @options={{this.options}}/>`);
-    await testSelectKeyboardSelection(assert, {
-      trigger: SelectPageObject.trigger,
-      list: SelectPageObject.list
+    test('select is invoked', async function (this: TestContext, assert) {
+      this.options = ['apple', 'banana', 'pineapple'];
+      this.select = sinon.spy();
+      await render(
+        hbs`<Select @options={{this.options}} @update={{this.select}}/>`
+      );
+      await SelectPageObject.select('apple');
+
+      assert.ok(this.select.calledOnceWith('apple'));
+    });
+
+    test('it supports keyboard controls for open and close', async function (this: TestContext, assert) {
+      await render(hbs`<Select/>`);
+      await testSelectKeyboardOpenAndClose(assert, {
+        trigger: SelectPageObject.trigger,
+        list: SelectPageObject.list
+      });
+    });
+
+    test('it supports keyboard navigation', async function (this: TestContext, assert) {
+      this.options = ['apple', 'banana', 'pineapple'];
+      await render(hbs`<Select @options={{this.options}}/>`);
+      await testSelecKeyboardNavigation(assert, {
+        trigger: SelectPageObject.trigger,
+        list: SelectPageObject.list
+      });
+    });
+
+    test('it supports keyboard selection', async function (this: TestContext, assert) {
+      this.options = ['apple', 'banana', 'pineapple'];
+      await render(hbs`<Select @options={{this.options}}/>`);
+      await testSelectKeyboardSelection(assert, {
+        trigger: SelectPageObject.trigger,
+        list: SelectPageObject.list
+      });
     });
   });
 });

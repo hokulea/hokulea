8746b0a664fc9804ad023fab5a096facdbd1dd08
gossi <gossi@users.noreply.github.com>
2021-03-13 08:55:37 +0100

    enable coverage

--- packages/inputs/ember-cli-build.js
+++ packages/inputs/ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    // Add options here
+    babel: {
+      sourceMaps: 'inline'
+    },
+    sourcemaps: {
+      enabled: true,
+      extensions: ['js']
+    }
   });
 
   /*

64e0c002db9fb58af08bee1b1bba28bea65babbb
gossi <gossi@users.noreply.github.com>
2022-07-02 16:39:27 +0200

    fix netlify build

--- packages/buttons/addon/api.d.ts
+++ packages/buttons/addon/api.d.ts
@@ -5,6 +5,3 @@
  */
 
 export { default as ButtonComponent } from '@hokulea/buttons/components/button';
-export { default as AlternativeButtonComponent } from '@hokulea/buttons/components/alternative-button';
-export { default as HighlightButtonComponent } from '@hokulea/buttons/components/highlight-button';
-export { default as DangerButtonComponent } from '@hokulea/buttons/components/danger-button';

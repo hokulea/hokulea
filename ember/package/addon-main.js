a593eb357c7758f2bee47d61b6e5bc41cbe5156b
Thomas Gossmann <gossi@users.noreply.github.com>
2024-07-18 04:30:00 +0200

    Fix release (#176)

--- ember/package/addon-main.js
+++ ember/package/addon-main.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const { addonV1Shim } = require('@embroider/addon-shim');
-// eslint-disable-next-line n/no-unpublished-require
 const { HokuleaCssModulesPlugin } = require('./lib/css-modules-plugin.js');
-// eslint-disable-next-line n/no-unpublished-require
 const { importAssets } = require('./lib/index.js');
 
 function isEngine(parent) {

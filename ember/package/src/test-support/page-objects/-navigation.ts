14f8dccc6e813e3955fd81c0ef748faa58ed5875
gossi <gossi@users.noreply.github.com>
2025-08-30 12:07:45 +0200

    tests

---
+++ ember/package/src/test-support/page-objects/-navigation.ts
@@ -0,0 +1,17 @@
+import { PageObject, selector as sel } from 'fractal-page-object';
+
+import { IconPageObject } from './icon';
+
+import type { ElementLike } from 'fractal-page-object';
+
+export class NavLinkPageObject extends PageObject<
+  HTMLButtonElement | HTMLAnchorElement | HTMLSpanElement
+> {
+  static SELECTOR = '[part="item"]';
+
+  constructor(selector?: string, parent?: PageObject | ElementLike | null, index?: number | null) {
+    super(selector ?? NavLinkPageObject.SELECTOR, parent, index);
+  }
+
+  $icon = sel(IconPageObject.SELECTOR, IconPageObject);
+}

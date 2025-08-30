de181ecb20957a607d65d9391cfa9a46419203c0
gossi <gossi@users.noreply.github.com>
2025-08-30 17:47:06 +0200

    with ext

--- ember/package/src/registry.ts
+++ ember/package/src/registry.ts
@@ -2,7 +2,7 @@ import { LinkManagerService } from 'ember-link';
 import PortalService from 'ember-stargate/services/-portal';
 import { buildRegistry } from 'ember-strict-application-resolver/build-registry';
 
-import HokuleaService from './services/-hokulea';
+import HokuleaService from './services/-hokulea.ts';
 
 export const hokuleaRegistry = buildRegistry({
   './services/-hokulea': HokuleaService,

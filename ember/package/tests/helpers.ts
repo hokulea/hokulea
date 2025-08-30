92c0795fc2055b61e440c382033a86376544a884
gossi <gossi@users.noreply.github.com>
2025-08-30 17:54:17 +0200

    fix lint

--- ember/package/tests/helpers.ts
+++ ember/package/tests/helpers.ts
@@ -5,7 +5,7 @@ import {
   setupTest as upstreamSetupTest
 } from 'ember-qunit';
 
-import type EmberResolver from 'ember-resolver';
+import type { Resolver } from '@ember/owner';
 
 // @ts-expect-error this __CI__ is set by vite at build time
 export const testButNotOnCI = __CI__ ? skip : test;
@@ -14,7 +14,7 @@ interface SetupTestOptions {
   /**
    * The resolver to use when instantiating container-managed entities in the test.
    */
-  resolver?: EmberResolver | undefined;
+  resolver?: Resolver | undefined;
 }
 
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's

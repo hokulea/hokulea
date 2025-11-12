24b402f2826655cc7bdea01be93db3c150078ecd
gossi <gossi@users.noreply.github.com>
2025-11-12 20:32:58 +0100

    Allow types for scoped-css

--- ember/package/types/index.d.ts
+++ ember/package/types/index.d.ts
@@ -1,6 +1,18 @@
+import '@glint/template';
+
 import type HokuleaRegistry from '../src/template-registry';
 import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry extends HokuleaRegistry, EmberElementHelperRegistry {}
+}
+
+/**
+ * Allow `scoped` attributes for `<style>`
+ */
+declare global {
+  interface HTMLStyleElementAttributes {
+    scoped: '';
+    inline: '';
+  }
 }

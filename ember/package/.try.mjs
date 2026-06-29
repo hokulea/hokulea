5e9b58c43e2b648f97100207fff8d5ac6db808a8
gossi <gossi@users.noreply.github.com>
2026-06-29 08:41:09 +0200

    deactivate ember v7 for now (not yet)

--- ember/package/.try.mjs
+++ ember/package/.try.mjs
@@ -7,30 +7,31 @@ export default {
           'ember-source': `npm:ember-source@~6.4.0`
         }
       }
-    },
-    {
-      name: `ember-latest`,
-      npm: {
-        devDependencies: {
-          'ember-source': `npm:ember-source@latest`
-        }
-      }
-    },
-    {
-      name: `ember-beta`,
-      npm: {
-        devDependencies: {
-          'ember-source': `npm:ember-source@beta`
-        }
-      }
-    },
-    {
-      name: `ember-alpha`,
-      npm: {
-        devDependencies: {
-          'ember-source': `npm:ember-source@alpha`
-        }
-      }
     }
+    // deactivate ember v7 below (not yet)
+    // {
+    //   name: `ember-latest`,
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': `npm:ember-source@latest`
+    //     }
+    //   }
+    // },
+    // {
+    //   name: `ember-beta`,
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': `npm:ember-source@beta`
+    //     }
+    //   }
+    // },
+    // {
+    //   name: `ember-alpha`,
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': `npm:ember-source@alpha`
+    //     }
+    //   }
+    // }
   ]
 };
